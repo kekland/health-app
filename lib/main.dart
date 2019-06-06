6f2ac93898b8b7d9126773491d656676e22c9d10
kekland <kk.erzhan@gmail.com>
2019-06-06 23:30:44 +0600

    finally working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:health_app/pages/login_page.dart';
 import 'package:health_app/pages/main_page.dart';
@@ -8,6 +9,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
+    FirebaseMessaging().configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Salut - здоровье',
